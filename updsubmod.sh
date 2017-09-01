946c9420bf7d34eaa7d14908abc9e9f70647fc3e
davey <davidralphhughes@college.harvard.edu>
2017-09-01 12:07:47 -0400

    Adds submodule init line to updsubmod.sh

--- updsubmod.sh
+++ updsubmod.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# initialize all submodules
+git submodule update --init --recursive
+
 # pull newest changes for each submodule, recursively
 git submodule foreach --recursive git checkout master
 git submodule foreach --recursive git pull
