c0030ed24f5d5dee142c451be625f75772be4444
Davey Hughes <davidralphhughes@gmail.com>
2019-07-24 16:50:15 -0700

    removes YouCompleteMe, VimCompletesMe, kite, glsl
    Also removes gruvbox colorscheme

--- updsubmod.sh
+++ updsubmod.sh
@@ -6,7 +6,3 @@ git submodule update --init --recursive
 # pull newest changes for each submodule, recursively
 git submodule foreach --recursive git checkout master
 git submodule foreach --recursive git pull
-
-# change YouCompleteMe submodules to expected versions
-cd ./bundle/YouCompleteMe
-git submodule update --init --recursive
