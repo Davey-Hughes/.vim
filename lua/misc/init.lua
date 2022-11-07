e74aeeac22167ac7cb216177264cdcd0fef37e03
davey <davidralphhughes@gmail.com>
2022-11-07 13:07:26 -0800

    Add plenary and nvim-coverage

--- lua/misc/init.lua
+++ lua/misc/init.lua
@@ -1,3 +1,31 @@
+vim.cmd [[packadd plenary.nvim]]
+local async = require'plenary.async'
+
+vim.cmd [[packadd nvim-coverage]]
+require('coverage').setup({
+  commands = true,
+
+  highlights = {
+      -- customize highlight groups created by the plugin
+      -- supports style, fg, bg, sp (see :h highlight-gui)
+      covered = { fg = "#C3E88D" },
+      uncovered = { fg = "#F07178" },
+  },
+
+  signs = {
+      -- use your own highlight groups or text markers
+      covered = { hl = "CoverageCovered", text = "▎" },
+      uncovered = { hl = "CoverageUncovered", text = "▎" },
+  },
+
+  summary = {
+      -- customize the summary pop-up
+      min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+  },
+
+  lang = {},
+})
+
 vim.cmd [[packadd nvim-numbertoggle]]
 require'numbertoggle'.setup ({})
 
