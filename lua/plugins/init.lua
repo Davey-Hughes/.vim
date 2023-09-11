5a0e192ef6bce0aeaa108ffee6c38f759f83eff7
Davey Hughes <davidralphhughes@gmail.com>
2023-09-10 21:01:19 -0700

    First Lazy.nvim commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,53 @@
+return {
+  -- colorscheme
+  "Davey-Hughes/NeoSolarized",
+
+  -- filetype
+  "ehamberg/vim-cute-python",
+  "udalov/kotlin-vim",
+  "fatih/vim-go",
+  "kchmck/vim-coffee-script",
+  "rust-lang/rust.vim",
+  "PyGamer0/vim-apl",
+  "bfrg/vim-cpp-modern",
+  "leafgarland/typescript-vim",
+  "nickeb96/fish.vim",
+  "maxbane/vim-asm_ca65",
+  "chrisbra/csv.vim",
+  "vim-ruby/vim-ruby",
+  "lervag/vimtex",
+
+  -- formatters
+  "rhysd/vim-clang-format",
+  "tell-k/vim-autopep8",
+  "prettier/vim-prettier",
+
+  -- fzf
+  "junegunn/fzf.vim",
+  "yuki-yano/fzf-preview.vim",
+
+  -- git
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
+
+  -- misc
+  "tpope/vim-sleuth",
+
+  -- ui
+  "mbbill/undotree",
+  "mkitt/tabline.vim",
+  "ipod825/vim-tabdrop",
+  "liuchengxu/vista.vim",
+  "ryanoasis/vim-devicons",
+  "sitiom/nvim-numbertoggle",
+
+  -- text editing
+  "scrooloose/nerdcommenter",
+  "tpope/vim-speeddating",
+  "tpope/vim-surround",
+  "tpope/vim-unimpaired",
+
+  -- tmux
+  "christoomey/vim-tmux-navigator",
+  "tmux-plugins/vim-tmux-focus-events",
+}
