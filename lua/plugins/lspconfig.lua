81bda5096ce58444adb44d1792b90584f03061c6
Davey Hughes <davidralphhughes@gmail.com>
2023-09-13 07:01:50 -0700

    Add rust-tools config and change NerdCommenter to Comment.nvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,31 +2,12 @@ return {
   "neovim/nvim-lspconfig",
 
   config = function()
-    require("lspconfig").rust_analyzer.setup {
-      on_attach = on_attach,
-      settings = {
-        ["rust-analyzer"] = {
-          assist = {
-            importMergeBehavior = "last",
-            importPrefix = "by_self",
-          },
-          diagnostics = {
-            disabled = { "unresolved-import" },
-          },
-          cargo = {
-            loadOutDirsFromCheck = true,
-          },
-          procMacro = {
-            enable = true,
-          },
-          checkOnSave = {
-            command = "clippy",
-          },
-        },
-      },
-    }
-
     require("lspconfig").pyright.setup {}
     require("lspconfig").ccls.setup {}
+
+    vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float)
+    vim.keymap.set("n", "<leader>l[", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "<leader>l]", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist)
   end,
 }
