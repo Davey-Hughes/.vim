a27e3395c314496929bade067221d2da0291c955
Davey Hughes <davidralphhughes@gmail.com>
2023-09-26 21:14:20 -0700

    Add LazyGit plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,6 +17,15 @@ return {
     config = true,
   },
   {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", nil)
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
