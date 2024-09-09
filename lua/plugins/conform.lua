dffdc57f2b2fcc8d41dc2e4a59e8907c1521eaae
davey <davidralphhughes@gmail.com>
2024-09-09 01:28:51 -0700

    Add full conform setup and remove formatter.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,25 +1,66 @@
 return {
-	"stevearc/conform.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				fish = { "fish_indent" },
-				json = { "prettierd", "prettier", stop_after_first = true },
-				kotlin = { "ktlint" },
-				lua = { "stylua" },
-				sh = { "shfmt" },
-				sql = { "sqlfluff" },
-				tex = { "latexindent" },
-				toml = { "taplo" },
-				yaml = { "prettierd" },
-				zig = { "zigfmt" },
-			},
+  "stevearc/conform.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        fish = { "fish_indent" },
+        graphql = { "prettierd" },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        kotlin = { "ktlint" },
+        latex = { "latexindent" },
+        lua = { "stylua" },
+        sh = { "shfmt" },
+        sql = { "sqlfluff" },
+        tex = { "latexindent" },
+        toml = { "taplo" },
+        yaml = { "prettierd" },
+        zig = { "zigfmt" },
+      },
 
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
-	end,
+      formatters = {
+        ["clang-format"] = {
+          args = {
+            "-assume-filename",
+            "$FILENAME",
+            "--style",
+            "file:" .. vim.env.HOME .. "/.vim/configs/.clang-format",
+          },
+        },
+        sqlfluff = {
+          args = {
+            "format",
+            "--dialect",
+            "postgres",
+            "--disable-progress-bar",
+            "--nocolor",
+            "-",
+          },
+          require_cwd = false,
+          exit_codes = { 0, 1 },
+        },
+
+        stylua = {
+          args = {
+            "--search-parent-directories",
+            "--stdin-filepath",
+            "$FILENAME",
+            "--indent-type",
+            "Spaces",
+            "--indent-width",
+            "2",
+            "--",
+            "-",
+          },
+        },
+      },
+
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end,
 }
