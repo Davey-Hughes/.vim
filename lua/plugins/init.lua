6c82bffc400721b15781e8972f02eeac819136e4
Davey Hughes <davidralphhughes@gmail.com>
2024-09-09 04:01:09 -0700

    Configure Trouble.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
   { "apple/pkl-neovim", ft = "pkl" },
   { "bfrg/vim-cpp-modern", ft = "cpp" },
   { "chrisbra/csv.vim", ft = "csv" },
-  { "ehamberg/vim-cute-python", ft = "python" },
   { "leafgarland/typescript-vim", ft = "typescript" },
   { "maxbane/vim-asm_ca65", ft = "ca65" },
   { "PyGamer0/vim-apl", ft = "apl" },
@@ -184,8 +183,42 @@ return {
   },
   {
     "folke/trouble.nvim",
-    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    cmd = "Trouble",
+    opts = {},
+    keys = {
+      {
+        "<leader>tx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>ts",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>tL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>tQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
   { "folke/twilight.nvim", event = "VeryLazy" },
   {
