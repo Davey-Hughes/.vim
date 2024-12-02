e97fea0852b2b0eab48af55edee2d87ce14ddaca
davey <davidralphhughes@gmail.com>
2024-12-02 02:34:35 -0800

    Add neogit and comment out fugitive and vim flog

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,6 @@
 return {
-  { "rbong/vim-flog", event = "VeryLazy" },
-  { "tpope/vim-fugitive", event = "VeryLazy" },
+  -- { "rbong/vim-flog", event = "VeryLazy" },
+  -- { "tpope/vim-fugitive", event = "VeryLazy" },
   { "tpope/vim-rhubarb", event = "VeryLazy" },
 
   {
@@ -13,6 +13,17 @@ return {
       vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", nil)
     end,
   },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = { graph_style = "kitty" },
+  },
+
   {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
