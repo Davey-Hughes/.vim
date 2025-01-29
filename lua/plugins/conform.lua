c5a8094ee0e892fab2e901eaa47cf26c69a02931
Davey Hughes <davidralphhughes@gmail.com>
2025-01-29 11:18:46 -0800

    Conform timeout 500 -> 1000

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -38,7 +38,7 @@ return {
         sh = { "shfmt" },
         sql = { "sqlfluff" },
         tex = { "latexindent" },
-        typescript = { "prettierd", "eslint_d", async = true },
+        typescript = { "prettierd", "eslint_d" },
         typescriptreact = { "prettierd" },
         toml = { "taplo" },
         uiua = { "uiua" },
@@ -98,7 +98,7 @@ return {
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         end
-        return { timeout_ms = 500, lsp_format = "fallback" }
+        return { timeout_ms = 1000, lsp_format = "fallback" }
       end,
     })
   end,
