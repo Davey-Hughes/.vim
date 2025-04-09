219b8f1f3d3f802191be12084cf38fb8dd4f979f
davey <davidralphhughes@gmail.com>
2025-04-09 10:04:06 -0700

    change local to .local/state/nvim and small changes to snacks/others

--- lua/plugins/filetype.lua
+++ lua/plugins/filetype.lua
@@ -18,11 +18,4 @@ return {
       vim.g.vimtex_format_enabled = true
     end,
   },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = "markdown",
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
 }
