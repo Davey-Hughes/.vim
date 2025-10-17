e2e3ea2cbc136f3d609468b3e16b379c226811c2
Davey Hughes <davidralphhughes@gmail.com>
2025-10-17 13:33:59 -0700

    update mcphub settings in codecompanion extensions

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -199,9 +199,13 @@ return {
         mcphub = {
           callback = "mcphub.extensions.codecompanion",
           opts = {
+            make_tools = true,
+            show_server_tools_in_chat = true,
+            add_mcp_prefix_to_tool_names = false,
+            show_result_in_chat = true,
+            format_tool = nil,
             make_vars = true,
             make_slash_commands = true,
-            show_result_in_chat = true,
           },
         },
 
