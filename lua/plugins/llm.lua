90f04ecf5ee81466d1c1903383f2cffd478e61e2
davey <davidralphhughes@gmail.com>
2025-10-17 17:51:49 -0700

    add vectorcode codecompanion extension

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,3 +1,5 @@
+---@module "vectorcode"
+
 return {
   {
     "olimorris/codecompanion.nvim",
@@ -293,19 +295,64 @@ return {
             },
           },
         },
+
+        vectorcode = {
+          ---@type VectorCode.CodeCompanion.ExtensionOpts
+          opts = {
+            tool_group = {
+              -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
+              enabled = true,
+              -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
+              -- if you use @vectorcode_vectorise, it'll be very handy to include
+              -- `file_search` here.
+              extras = {},
+              collapse = false, -- whether the individual tools should be shown in the chat
+            },
+            tool_opts = {
+              ---@type VectorCode.CodeCompanion.ToolOpts
+              ["*"] = {},
+              ---@type VectorCode.CodeCompanion.LsToolOpts
+              ls = {},
+              ---@type VectorCode.CodeCompanion.VectoriseToolOpts
+              vectorise = {},
+              ---@type VectorCode.CodeCompanion.QueryToolOpts
+              query = {
+                max_num = { chunk = -1, document = -1 },
+                default_num = { chunk = 50, document = 10 },
+                include_stderr = false,
+                use_lsp = true,
+                no_duplicate = true,
+                chunk_mode = false,
+                ---@type VectorCode.CodeCompanion.SummariseOpts
+                summarise = {
+                  ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
+                  enabled = false,
+                  adapter = nil,
+                  query_augmented = true,
+                },
+              },
+              files_ls = {},
+              files_rm = {},
+            },
+          },
+        },
       },
+    },
+  },
+
+  {
+    "Davidyz/VectorCode",
+    version = "*",
+    build = "uv tool upgrade vectorcode",
+    opts = {
+      async_backend = "default",
     },
   },
 
   {
     "milanglacier/minuet-ai.nvim",
     enabled = false,
-    dependencies = {
-      {
-        { "nvim-lua/plenary.nvim" },
-        { "Davidyz/VectorCode", version = "*", build = "uv tool upgrade vectorcode" },
-      },
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
       ---@diagnostic disable-next-line: missing-fields
