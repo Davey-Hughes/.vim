3c940645bbd747dd5942b2f7659fafac730c54e9
Davey Hughes <davidralphhughes@gmail.com>
2026-03-01 15:48:13 -0500

    disable mcphub

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -30,6 +30,7 @@ return {
       },
       {
         "ravitemer/mcphub.nvim",
+        enabled = false,
         build = "bundled_build.lua",
         opts = {
           use_bundled_binary = true,
@@ -168,18 +169,18 @@ return {
       },
 
       extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_tools = true,
-            show_server_tools_in_chat = true,
-            add_mcp_prefix_to_tool_names = false,
-            show_result_in_chat = true,
-            format_tool = nil,
-            make_vars = true,
-            make_slash_commands = true,
-          },
-        },
+        -- mcphub = {
+        --   callback = "mcphub.extensions.codecompanion",
+        --   opts = {
+        --     make_tools = true,
+        --     show_server_tools_in_chat = true,
+        --     add_mcp_prefix_to_tool_names = false,
+        --     show_result_in_chat = true,
+        --     format_tool = nil,
+        --     make_vars = true,
+        --     make_slash_commands = true,
+        --   },
+        -- },
 
         history = {
           enabled = true,
@@ -266,52 +267,53 @@ return {
           },
         },
 
-        vectorcode = {
-          ---@type VectorCode.CodeCompanion.ExtensionOpts
-          opts = {
-            tool_group = {
-              -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
-              enabled = true,
-              -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
-              -- if you use @vectorcode_vectorise, it'll be very handy to include
-              -- `file_search` here.
-              extras = {},
-              collapse = false, -- whether the individual tools should be shown in the chat
-            },
-            tool_opts = {
-              ---@type VectorCode.CodeCompanion.ToolOpts
-              ["*"] = {},
-              ---@type VectorCode.CodeCompanion.LsToolOpts
-              ls = {},
-              ---@type VectorCode.CodeCompanion.VectoriseToolOpts
-              vectorise = {},
-              ---@type VectorCode.CodeCompanion.QueryToolOpts
-              query = {
-                max_num = { chunk = -1, document = -1 },
-                default_num = { chunk = 50, document = 10 },
-                include_stderr = false,
-                use_lsp = true,
-                no_duplicate = true,
-                chunk_mode = false,
-                ---@type VectorCode.CodeCompanion.SummariseOpts
-                summarise = {
-                  ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
-                  enabled = false,
-                  adapter = nil,
-                  query_augmented = true,
-                },
-              },
-              files_ls = {},
-              files_rm = {},
-            },
-          },
-        },
+        -- vectorcode = {
+        --   ---@type VectorCode.CodeCompanion.ExtensionOpts
+        --   opts = {
+        --     tool_group = {
+        --       -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
+        --       enabled = true,
+        --       -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
+        --       -- if you use @vectorcode_vectorise, it'll be very handy to include
+        --       -- `file_search` here.
+        --       extras = {},
+        --       collapse = false, -- whether the individual tools should be shown in the chat
+        --     },
+        --     tool_opts = {
+        --       ---@type VectorCode.CodeCompanion.ToolOpts
+        --       ["*"] = {},
+        --       ---@type VectorCode.CodeCompanion.LsToolOpts
+        --       ls = {},
+        --       ---@type VectorCode.CodeCompanion.VectoriseToolOpts
+        --       vectorise = {},
+        --       ---@type VectorCode.CodeCompanion.QueryToolOpts
+        --       query = {
+        --         max_num = { chunk = -1, document = -1 },
+        --         default_num = { chunk = 50, document = 10 },
+        --         include_stderr = false,
+        --         use_lsp = true,
+        --         no_duplicate = true,
+        --         chunk_mode = false,
+        --         ---@type VectorCode.CodeCompanion.SummariseOpts
+        --         summarise = {
+        --           ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
+        --           enabled = false,
+        --           adapter = nil,
+        --           query_augmented = true,
+        --         },
+        --       },
+        --       files_ls = {},
+        --       files_rm = {},
+        --     },
+        --   },
+        -- },
       },
     },
   },
 
   {
     "Davidyz/VectorCode",
+    enabled = false,
     version = "*",
     build = "uv tool upgrade vectorcode",
     opts = {
