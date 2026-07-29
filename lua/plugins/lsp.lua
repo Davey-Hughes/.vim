c5e5d367b981a10c833f9e99628da7c5f5afd1cf
davey <davidralphhughes@gmail.com>
2026-07-29 03:04:33 -0700

    use buf instead of the deprecated buffer autocmd key
    nvim 0.13 renamed the "buffer" key accepted by nvim_create_autocmd() to "buf".
    Verified accepted by both 0.12.4 and 0.13.0-dev, so this is safe on either.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -155,7 +155,7 @@ return {
       eslint = {
         on_attach = function(_, bufnr)
           vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
+            buf = bufnr,
             command = "EslintFixAll",
           })
         end,
